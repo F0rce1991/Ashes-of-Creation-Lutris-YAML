54be64575238829f602194af4a5ca759a2e5cb06
F0rce1991 <129335429+F0rce1991@users.noreply.github.com>
2024-11-15 21:47:29 +0100

    Delete aoc-port-updater.sh
    Removed the file because it don't work like intended.

--- aoc-port-updater.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Endless loop which updates the "LauncherTetherPort" variable every second.
-# The idea behind the endless loop is to ensure port updates even after logging out and back in again.
-while :
-do
-    LauncherTetherPort=$(netstat -ulpn | grep wineserv | awk '{split($4, a , ":"); print a[2]}')
-    sleep 1
-done
